df102e6be38e0be33008c46277c7ae51a3186b47
SeedPaths-User <lessdismissive@email.com>
2014-04-25 13:54:47 -0600

    objectPractice

---
+++ objectPractice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectPractice
+{
+    class bird
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+        }
+        public class bird
+        {
+            public string birdColor();
+            public string birdGender();
+            public int numberOfBirds();
+
+
+
+
+        }
+    }
+}
